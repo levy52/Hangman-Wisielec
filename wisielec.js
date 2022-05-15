17cdb85b0ff3e048f74c6dc07ccbfd7ba7fcac21
levy52 <radeklewicki52@gmail.com>
2022-05-15 12:49:13 +0200

    code reformat in letters

--- wisielec.js
+++ wisielec.js
@@ -18,43 +18,7 @@ function write_password() {
 
 window.onload = start;
 
-var letters = new Array(35);
-
-letters[0] = "A";
-letters[1] = "Ą";
-letters[2] = "B";
-letters[3] = "C";
-letters[4] = "Ć";
-letters[5] = "D";
-letters[6] = "E";
-letters[7] = "Ę";
-letters[8] = "F";
-letters[9] = "G";
-letters[10] = "H";
-letters[11] = "I";
-letters[12] = "J";
-letters[13] = "K";
-letters[14] = "L";
-letters[15] = "Ł";
-letters[16] = "M";
-letters[17] = "N";
-letters[18] = "Ń";
-letters[19] = "O";
-letters[20] = "Ó";
-letters[21] = "P";
-letters[22] = "Q";
-letters[23] = "R";
-letters[24] = "S";
-letters[25] = "Ś";
-letters[26] = "T";
-letters[27] = "U";
-letters[28] = "V";
-letters[29] = "W";
-letters[30] = "X";
-letters[31] = "Y";
-letters[32] = "Z";
-letters[33] = "Ź";
-letters[34] = "Ż";
+var letters = "AĄBCĆDEĘFGHIJKLŁMNŃOÓPQRSŚTUVWXYZŹŻ";
 
 function start() {
     var content = "";
